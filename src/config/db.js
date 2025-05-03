8afa881ab6fc1b1bdcd3d244eb391216ca9fc42c
rEjIn2 <rejin374@gmail.com>
2025-05-03 09:39:35 +0400

    updated-mongodb-queries

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,35 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-// const mongoDbUrl='mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority';
-const mongoDbUrl=process.env.MONGO;
-const connectDb=()=>{
-    return mongoose.connect(mongoDbUrl)
-}
+// // const mongoDbUrl='mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority';
+// const mongoDbUrl=process.env.MONGO;
+// const connectDb=()=>{
+//     return mongoose.connect(mongoDbUrl)
+// }
 
-module.exports={connectDb}
+// module.exports={connectDb}
+
+const mongoose = require("mongoose");
+// const logger = require("./logger");
+// const { isDev } = require("./index");
+
+mongoose.Promise = global.Promise;
+
+const option = { useNewUrlParser: true, useUnifiedTopology: true };
+// if (isDev()) {
+//   option.autoIndex = false;
+// }
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, option);
+    // logger.info("MongoDB Connection ok!");
+    global.defaultSettings = { language: "eng" };
+    // mongoose.set("debug", true);
+    return true;
+  } catch (err) {
+    // logger.error(`MongoDB Connection error: ${err.message}`);
+    return false;
+  }
+};
+
+module.exports = {connectDb};
